0f79b70f7cd90dc9c593bc7cc3fd6552f1da5169
LEEJIHO <jiho264@naver.com>
2023-04-30 04:21:27 +0900

    add ref and fix range

--- homework02/homework02.c
+++ homework02/homework02.c
@@ -14,25 +14,20 @@ typedef struct OBJECT
     int dx, dy;
 } OBJECT;
 
-OBJECT user = {20, 100, BOX_AZ, BOX_EL, 0, 15};
+OBJECT user = {20, 100, BOX_AZ, BOX_EL, 0, 25};
 OBJECT comp = {XLIM - 2 * BOX_AZ, 100, BOX_AZ, BOX_EL, 0, -1};
 OBJECT ball = {XLIM / 2 - 100, YLIM / 2, BALL_2R, BALL_2R, -1, -1};
 
 // 도형 그리기
 void Draw_Object(HWND hWnd, HDC hdc, HBRUSH hBrush, OBJECT prt, BOOL _is_rect)
 {
-
     HBRUSH oBrush;                              // DC에 기존 선택 브러쉬 핸들 기억할 변수
     oBrush = (HBRUSH)SelectObject(hdc, hBrush); // 입력 인자로 전달받은 브러쉬를 DC에 선택
-
     if (_is_rect == 1)
         Rectangle(hdc, prt.x, prt.y, prt.x + BOX_AZ, prt.y + BOX_EL); // 사각형 그리기
     else
         Ellipse(hdc, prt.x, prt.y, prt.x + BALL_2R, prt.y + BALL_2R); // 타원 그리기
-
-    RECT _tmp = {prt.x, prt.y, prt.x + prt.az, prt.y + prt.el};
-    InvalidateRect(hWnd, &_tmp, TRUE);
-
+    InvalidateRect(hWnd, 0, TRUE);
     SelectObject(hdc, oBrush); // 기존 선택 브러쉬를 DC에 선택
 }
 // 그리기 작업
@@ -40,13 +35,10 @@ void Draw_ALL(HWND hWnd)
 {
     // 츨력할 좌표 설정
     // TextOut(hdc, 100, 100, "x", 1);
-
     PAINTSTRUCT ps;
     BeginPaint(hWnd, &ps);
-
     Draw_Object(hWnd, ps.hdc, CreateSolidBrush(RGB(255, 0, 0)), user, 1);
     Draw_Object(hWnd, ps.hdc, CreateSolidBrush(RGB(0, 255, 0)), comp, 1);
-    // 공 날라갈 때 조금 덜 그려짐
     Draw_Object(hWnd, ps.hdc, CreateSolidBrush(RGB(0, 0, 0)), ball, 0);
     EndPaint(hWnd, &ps);
 }
@@ -54,7 +46,6 @@ void Draw_ALL(HWND hWnd)
 /* key*/
 LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
-    int _upanddown = -999;
     switch (iMessage)
     {
     case WM_KEYDOWN:
@@ -63,7 +54,7 @@ LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPara
         {
         case VK_UP:
         {
-            // bar가 경계 닿는 순간
+            // bar가 경계 닿는 순간 이동 블로킹
             if (user.y <= 0)
             {
             }
@@ -73,7 +64,7 @@ LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPara
         }
         case VK_DOWN:
         {
-            // bar가 경계 닿는 순간
+            // bar가 경계 닿는 순간 이동 블로킹
             if (user.y + 1.4 * BOX_EL >= YLIM)
             {
             }
@@ -87,23 +78,37 @@ LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPara
     }
     case WM_PAINT:
     {
+
+        ball.x += ball.dx;
+        ball.y += ball.dy;
+
         // comp의 기본 움직임
-        // bar가 경계 닿는 순간
+        // bar가 경계 닿는 순간에 방향 반대로
         if (comp.y <= 0 || comp.y + 1.4 * BOX_EL >= YLIM)
         {
             comp.dy = -comp.dy;
         }
         comp.y += comp.dy;
 
-        // 만약 공이 user에 닿음
+        // 만약 공이 user에 닿음 > 튕기고 속도 2배
         if (ball.x <= user.x + BOX_AZ)
         {
             if (user.y < ball.y && ball.y + BALL_2R < user.y + BOX_EL)
             {
                 ball.dx = -ball.dx;
+                // level up!
+                if (ball.dx >= 0)
+                    ball.dx += 1;
+                else
+                    ball.dx -= 1;
+
+                if (ball.dy >= 0)
+                    ball.dx += 1;
+                else
+                    ball.dy -= 1;
             }
         }
-        // 만약 공이 comp에 닿음
+        // 만약 공이 comp에 닿음 > 그냥 튕기기만
         if (comp.x <= ball.x + BALL_2R)
         {
             if (comp.y < ball.y && ball.y + BALL_2R < comp.y + BOX_EL)
@@ -112,25 +117,44 @@ LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPara
             }
         }
         // ball이 위나 아래에 부딪히면 반대로
-        if (ball.y == 0 || ball.y + 3.5 * BALL_2R >= YLIM)
+        if (ball.y < 0 || ball.y + 3.5 * BALL_2R >= YLIM)
         {
             ball.dy = -ball.dy;
+        }
+        // 만약 user가 놓침 > init game
+        if (ball.x <= 0)
+        {
+            // iniy ball
+            ball.x = 500;
+            ball.y = 250;
+            ball.dx = -1;
+            ball.dy = -1;
+            comp.dy = 1;
         }
         // 만약 comp가 ball을 놓쳐서 오른쪽 바깥으로 나감
         if (XLIM <= ball.x + BALL_2R)
         {
             // level up!
-            comp.dy *= 2;
+            if (comp.dy >= 0)
+                comp.dy += 1;
+            else
+                comp.dy -= 1;
+
+            if (ball.dx >= 0)
+                ball.dx += 1;
+            else
+                ball.dx -= 1;
+
+            if (ball.dy >= 0)
+                ball.dx += 1;
+            else
+                ball.dy -= 1;
             // iniy ball
             ball.x = 500;
             ball.y = 250;
-            ball.dx *= -ball.dx;
-            ball.dy *= -ball.dy;
+            ball.dx = -ball.dx;
+            ball.dy = -ball.dy;
         }
-
-        ball.x += ball.dx;
-        ball.y += ball.dy;
-
         Draw_ALL(hWnd);
         return 0;
     }
@@ -140,8 +164,11 @@ LRESULT CALLBACK MyWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPara
     }
     return DefWindowProc(hWnd, iMessage, wParam, lParam);
 }
-
-INT APIENTRY WinMain(HINSTANCE hIns, HINSTANCE hPrev, LPSTR cmd, INT nShow)
+INT APIENTRY WinMain(
+    _In_ HINSTANCE hIns,
+    _In_opt_ HINSTANCE hPrev,
+    _In_ LPSTR cmd,
+    _In_ INT nShow)
 {
     WNDCLASS wndclass = {0};
     wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); // 흰색 브러쉬 핸들
@@ -174,3 +201,9 @@ INT APIENTRY WinMain(HINSTANCE hIns, HINSTANCE hPrev, LPSTR cmd, INT nShow)
     }
     return 0;
 }
+
+/*
+참고 블로그
+https://ehpub.co.kr/7-%ec%b2%ab-%eb%b2%88%ec%a7%b8-%ec%8b%a4%ec%8a%b5-%eb%8f%84%ed%98%95-%ec%9d%b4%eb%8f%99-%ec%8b%9c%ed%82%a4%ea%b8%b0/
+
+*/
