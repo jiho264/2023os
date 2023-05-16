51175b70384f28a14067945bbf911397cff57773
LEEJIHO <jiho264@naver.com>
2023-05-16 14:24:40 +0900

    dcscnalsncl

--- masm/_sub1.asm
+++ masm/_sub1.asm
@@ -2,18 +2,21 @@ CODE SEGMENT
     ASSUME CS:CODE
     MOV CL, 0 ; INIT
 NEXT: 
-    CALL PUTNUM ; CALL SUBROUTINE
+    CALL PUTNUM ; CALL SUBROUTINE ; 되돌아올 두소 0005h를 stack에 pysh
     INC CL ; CL ++
-    CMP CL, 10 ; IF CL < 10
+    CMP CL, 26 ; IF CL < 10
     JB NEXT ; GO TO NEXT
 
     MOV AH, 4CH
     INT 21H
 PUTNUM:
     MOV DL, CL
-    ADD DL, '0' ; INT2STR
+    ADD DL, 'a' ; INT2STR
     MOV AH, 2
     INT 21H
-    RET
+    RET ; stack에서 pop해서 0005H를 ip값으로 바꿈
 CODE ENDS
 END
+
+; 응용문제 >> 거꾸로 출력하기, 홀수 or 짝수만 출력해보기
+; 모음은 출력하지 말아라
